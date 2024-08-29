b482df9f81e7fe503a55c50c2005aeccaf51727a
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-08-29 09:24:00 -0300

    treino - trabalhando com arquivos file e exception

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,33 @@
 package application;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
+		File file = new File("c:\\temp\\in.txt");
+		
+		Scanner sc = null;
+		
+		try {
+			sc = new Scanner(file);
+			while(sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+			// sc.close()  --> não posso colocar aqui pois pode ocorrer uma exceção
+		}
+		catch (IOException e){
+			System.out.println("Error: " + e.getMessage());
+		}
+		finally {
+			if(sc != null) {
+				sc.close(); // --> certo é colocar o sc.close() aqui, porque o bloco finally será chamado mesmo que aconteça erros
+			}
+			
+		}
 
 	}
 
